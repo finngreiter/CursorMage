c504f24488ace6c6972e038017431fa5b7c5b9d2
Finn Greiter <finn.greiter@gmail.com>
2020-02-11 14:22:41 -0800

    Yay!

---
+++ CursorMage/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CursorMage
+{
+    partial class CMage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CMage));
+            this.CenterButton = new System.Windows.Forms.Button();
+            this.xNum = new System.Windows.Forms.NumericUpDown();
+            this.yNum = new System.Windows.Forms.NumericUpDown();
+            this.OKButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.xNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CenterButton
+            // 
+            this.CenterButton.Location = new System.Drawing.Point(80, 96);
+            this.CenterButton.Name = "CenterButton";
+            this.CenterButton.Size = new System.Drawing.Size(75, 23);
+            this.CenterButton.TabIndex = 0;
+            this.CenterButton.Text = "Center";
+            this.CenterButton.UseVisualStyleBackColor = true;
+            this.CenterButton.Click += new System.EventHandler(this.CenterButton_Click);
+            // 
+            // xNum
+            // 
+            this.xNum.Location = new System.Drawing.Point(73, 41);
+            this.xNum.Name = "xNum";
+            this.xNum.Size = new System.Drawing.Size(41, 20);
+            this.xNum.TabIndex = 3;
+            // 
+            // yNum
+            // 
+            this.yNum.Location = new System.Drawing.Point(120, 41);
+            this.yNum.Name = "yNum";
+            this.yNum.Size = new System.Drawing.Size(41, 20);
+            this.yNum.TabIndex = 4;
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(80, 67);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 6;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // CMage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 161);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.yNum);
+            this.Controls.Add(this.xNum);
+            this.Controls.Add(this.CenterButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CMage";
+            this.Text = "CursorMage";
+            this.Load += new System.EventHandler(this.CMage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.xNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yNum)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CenterButton;
+        private System.Windows.Forms.NumericUpDown xNum;
+        private System.Windows.Forms.NumericUpDown yNum;
+        private System.Windows.Forms.Button OKButton;
+    }
+}
+
---
+++ CursorMage/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursorMage
+{
+    public partial class CMage : Form
+    {
+        public CMage()
+        {
+            InitializeComponent();
+        }
+
+        private void CMage_Load(object sender, EventArgs e)
+        {
+            xNum.Maximum = Screen.PrimaryScreen.Bounds.Width;
+            yNum.Maximum = Screen.PrimaryScreen.Bounds.Height;
+        }
+
+        private void CenterButton_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(Convert.ToInt32(xNum.Value), Convert.ToInt32(yNum.Value));
+        }
+    }
+}
